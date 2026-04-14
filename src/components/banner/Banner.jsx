2a7ef1afd6ed832623150f6721a0d8d017f66bc2
MRrana26 <hafezmasudranamn@gmail.com>
2026-04-14 15:54:09 +0600

    banner section some updated for responsive all device

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,12 +5,12 @@ const Banner = () => {
     return (
         <div className='w-10/12 mx-auto'>
             <div className='text-center py-20'>
-                <h1 className=' text-[#1F2937] text-5xl font-bold'>Friends to keep close in your life</h1>
+                <h1 className=' text-3xl md:text-4xl lg:text-5xl font-bold text-[#1F2937]'>Friends to keep close in your life</h1>
                 <p className='text-[#64748B] mt-4'><small>Your personal shelf of meaningful connections. Browse, tend, and nurture the <br /> relationships that matter most.</small></p>
                 <button className='mt-8 btn bg-[#244D3F] shadow-sm text-white'><FaPlus /> Add a Friend</button>
             </div>
 
-            <div className='grid grid-cols-4 gap-3'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3'>
                 <div className='text-center bg-white rounded-2xl space-y-2 py-8 shadow-sm'>
                     <h1 className='text-3xl font-semibold text-[#244D3F]'>10</h1>
                     <p className='text-[18px] text-[#64748B]'>Total Friends</p>
