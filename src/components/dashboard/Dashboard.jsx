9d2eb9d3d2a5054e49788c3db1d8ffcbd76dddcc
MRrana26 <hafezmasudranamn@gmail.com>
2026-04-14 15:42:28 +0600

    banner section added and i try to designing like figma

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from '../banner/Banner';
 
 const Dashboard = () => {
     return (
-        <div>
-            Dashboard 
-        </div>
+        <>
+        <Banner />
+        </>
     );
 };
 
