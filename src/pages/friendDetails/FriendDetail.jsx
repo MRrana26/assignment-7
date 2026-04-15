1560ddcb4182bd1afef405ecc0e17390bb3fce80
MRrana26 <hafezmasudranamn@gmail.com>
2026-04-15 11:51:52 +0600

    vercel deploy problem try to solved

--- src/pages/friendDetails/FriendDetail.jsx
+++ src/pages/friendDetails/FriendDetail.jsx
@@ -22,35 +22,35 @@ const FriendDetail = () => {
 
     return (
         <>
-            {loading ? <div className='flex justify-center'><HashLoader color='#244D3F' /></div> :
-                <div>
+            {loading ? <div className='flex justify-center items-center min-h-[200px]'><HashLoader color='#244D3F' /></div> :
+                expectFriend && (
                     <div>
-                        <div className='flex justify-center items-center'>
-                            <img src={expectFriend.picture} alt="my friends image" className="w-20 h-20 object-cover object-center rounded-full" />
-                        </div>
-                        <h1 className='text-xl font-bold mt-2 text-[#244D3F]'>{expectFriend.name}</h1>
                         <div>
-                            {
-                                expectFriend.tags.map((tag, index) => (
-                                    <span key={index} className='uppercase font-semibold bg-[#CBFADB] px-3 py-1.5 rounded-full mx-1'>
-                                        {tag}
-                                    </span>
-                                ))
-                            }
+                            <div className='flex justify-center items-center'>
+                                <img src={expectFriend.picture} alt="my friends image" className="w-20 h-20 object-cover object-center rounded-full" />
+                            </div>
+                            <h1 className='text-xl font-bold mt-2 text-[#244D3F] text-center'>{expectFriend.name}</h1>
+                            <div className='flex justify-center my-2'>
+                                {
+                                    expectFriend.tags.map((tag, index) => (
+                                        <span key={index} className='uppercase font-semibold bg-[#CBFADB] px-3 py-1.5 rounded-full mx-1 text-[10px]'>
+                                            {tag}
+                                        </span>
+                                    ))
+                                }
+                            </div>
+
+                            <div className='text-center space-y-1'>
+                                <p>{expectFriend.email}</p>
+                                <p>{expectFriend.days_since_contact}</p>
+                                <p>{expectFriend.status}</p>
+                                <p>{expectFriend.bio}</p>
+                                <p>{expectFriend.goal}</p>
+                                <p>{expectFriend.next_due_date}</p>
+                            </div>
                         </div>
-
-                        <p>{expectFriend.email}</p>
-                        <p>{expectFriend.days_since_contact}</p>
-                        <p>{expectFriend.status}</p>
-                        <p>{expectFriend.bio}</p>
-                        <p>{expectFriend.goal}</p>
-                        <p>{expectFriend.next_due_date}</p>
-                        
-
                     </div>
-
-                    <div></div>
-                </div>
+                )
             }
         </>
     );
